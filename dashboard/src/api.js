864e73f09adea9f49b74509b971c34a58fd10341
dinoru <diarra.msa@gmail.com>
2025-07-07 17:28:43 +0300

    changed api

--- dashboard/src/api.js
+++ dashboard/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://31.129.111.73/api/v1',
+  baseURL: 'https://api.chapdemo.ru/api/v1',
   // baseURL: 'http://localhost:8001/api/v1/',
 });
 
